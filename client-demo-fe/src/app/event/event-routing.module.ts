ff6a4821170b0e72dcf9fb83a1379f2c5ddac287
Sofia Valente <sofia.valente@cmas-systems.com>
2019-01-17 16:08:59 +0000

    Remove of unnecessary resolver

--- client-demo-fe/src/app/event/event-routing.module.ts
+++ client-demo-fe/src/app/event/event-routing.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EventListComponent } from './event-list/event-list.component';
 import { EventItemComponent } from './event-item/event-item.component';
-import {EventResolver} from './event.resolver';
 
 const routes: Routes = [
   { path: 'events', component: EventListComponent },
-  { path: 'events/:id', component: EventItemComponent, resolve: {event: EventResolver} }
+  { path: 'events/:id', component: EventItemComponent }
 ];
 
 @NgModule({
